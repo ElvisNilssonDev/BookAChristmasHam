34dff41776ae31cab5c3b40d9a1d90828d0289a4
qubicbit <sis.esse161@outlook.com>
2025-11-06 13:36:25 +0100

    Cretaed an interface. Implemented in the Generik class, DataStore.

---
+++ Interfaces/IHasId.cs
@@ -0,0 +1,10 @@
+﻿
+namespace BookAChristmasHam.Interfaces
+{
+    public interface IHasId
+    {
+        // Alla objekt som implementerar detta interface måste ha en Id property av typen int.
+        // alltså en identifierare för objektet.
+        int id { get; set; }
+    }
+}
