59a02d3c416c69eddcfb52a93df36dea392975bf
natta1995 <natalie.hallerdal@yahoo.se>
2025-11-06 14:36:46 +0100

    Created Public Class Chrismasham

--- Models/ChristmasHam.cs
+++ Models/ChristmasHam.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace BookAChristmasHam.Models
 {
-    internal class ChristmasHam
+    // Klassen ChrismasHam representerar en julskinka som kan beställas av kunden
+    public class ChristmasHam
     {
+        // Unikt id för skinkan
+        private int Id { get; set; }
+
+        // Vikten på skinkan i kg
+        public int Weight { get; set; }
+
+        // True om skinkan är inlagd
+        public bool Brined { get; set; }
+
+        // True om skinkan har ben
+        public bool HasBones { get; set; }
+
+        // True om skinkan är kokt
+        public bool IsCooked { get; set; }
+
+        // Veckonummer då de vill ha skinkan
+        public int Week { get; set; }
+
+
+        // Konstruktor som används för att skapa ett ChrismasHam objekt
+        // Här skickar man in alla värden när man skapar skinkan
+        public ChristmasHam( int id, int weight, bool brined, bool hasBones, bool isCooked, int week) {
+            
+            Id = id;
+            Weight = weight;
+            Brined = brined;
+            HasBones = hasBones;
+            IsCooked = isCooked;
+            Week = week;
+
+            }
+
+
     }
 }
