a9b42b3c0f0094b5e554dfc172d99007472e052c
ElvisNilssonDev <Nilssonelvis@gmail.com>
2025-11-11 12:40:01 +0100

    Fully functioning User menu

--- UI/Menu/LoggedInMenu/PrivateMenu.cs
+++ UI/Menu/LoggedInMenu/PrivateMenu.cs
@@ -16,28 +16,38 @@ namespace BookAChristmasHam.UI.Menu.LoggedInMenu
 
             while (runningpriv)
             {
-                Console.Clear();
-                AnsiConsole.MarkupLine("[yellow]--- Book A christmasHam ---[/]");
-                AnsiConsole.MarkupLine("[green]1.[/] Book a Ham");
-                AnsiConsole.MarkupLine("[green]2.[/] Show My Order");
-                AnsiConsole.MarkupLine("[green]0.[/] Loggout");
-                AnsiConsole.MarkupLine("---------------------");
-                var choice = AnsiConsole.Ask<int>(":");
+                AnsiConsole.Clear();
+                AnsiConsole.Write(
+                new FigletText("ChristmasHam!")
+                .Centered()
+                .Color(Color.Red));
+                var choice = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title("[green]Select an option:[/]")
+                .PageSize(10)
+                .AddChoices(new[]
+                {
+                    "Order ChristmasHam",
+                    "See Your Order",
+                    "Logout"
+                }));
+
+                AnsiConsole.MarkupLine($"You selected: [yellow]{choice}[/]");
                 switch (choice)
                 {
-                    case 1:
+                    case "Order ChristmasHam":
                         // Visa mina bokningar
                         AnsiConsole.MarkupLine("[blue]Visa mina bokningar - Funktionalitet kommer snart![/]");
                         AnsiConsole.MarkupLine("Tryck på valfri tangent för att fortsätta...");
                         Console.ReadKey();
                         break;
-                    case 2:
+                    case "See Your Order":
                         // Ta bort en bokning
                         AnsiConsole.MarkupLine("[blue]Ta bort en bokning - Funktionalitet kommer snart![/]");
                         AnsiConsole.MarkupLine("Tryck på valfri tangent för att fortsätta...");
                         Console.ReadKey();
                         break;
-                    case 0:
+                    case "Logout":
                         // Logga ut
                         runningpriv = false;
                         AnsiConsole.MarkupLine("[green]You have logged out! See you soon!.[/]");
