877d1d943159a948ad52a82ca893d1dfe50406bf
Johnjr1 <114564748+Johnjr1@users.noreply.github.com>
2025-11-11 13:26:20 +0100

    placeholders for business menu

--- UI/Menu/LoggedInMenu/BusinessMenu.cs
+++ UI/Menu/LoggedInMenu/BusinessMenu.cs
@@ -24,16 +24,37 @@ namespace BookAChristmasHam.UI.Menu.LoggedInMenu
                 Console.Clear();
                 // Visar meny för företagsanvändare
                 AnsiConsole.MarkupLine($"[bold]Welcome, {businessUser.CompanyName}![/]");
+                AnsiConsole.MarkupLine("[yellow]--- Business menu ---[/]");
+                AnsiConsole.MarkupLine("[green]1.[/] Show all orders");
+                AnsiConsole.MarkupLine("[green]2.[/] Delete an order");
+                AnsiConsole.MarkupLine("[green]2.[/] Filter orders");
+                AnsiConsole.MarkupLine("[green]2.[/] Update an order");
+                AnsiConsole.MarkupLine("[green]0.[/] Log out");
+                AnsiConsole.MarkupLine("---------------------");
                 var choice = AnsiConsole.Ask<int>(":");
                 switch (choice)
                 {
                     case 1:
-                        // Visa mina bokningar
-                        AnsiConsole.MarkupLine("[blue]Show all ham[/]");
-                        AnsiConsole.MarkupLine("Tryck på valfri tangent för att fortsätta...");
+                        AnsiConsole.MarkupLine("[blue]ShowAllHam()...[/]");
+                        AnsiConsole.MarkupLine("Press any key to continue...");
                         Console.ReadKey();
                         break;
                     case 2:
+                        AnsiConsole.MarkupLine("[blue]DeleteOrder()...[/]");
+                        AnsiConsole.MarkupLine("Press any key to continue...");
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        AnsiConsole.MarkupLine("[blue]FilterOrder()...[/]");
+                        AnsiConsole.MarkupLine("Press any key to continue...");
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        AnsiConsole.MarkupLine("[blue]UppdateOrder()...[/]");
+                        AnsiConsole.MarkupLine("Press any key to continue...");
+                        Console.ReadKey();
+                        break;
+                    case 0:
                         runningbusiness = false; // Exit the loop
                         break;
                 }
