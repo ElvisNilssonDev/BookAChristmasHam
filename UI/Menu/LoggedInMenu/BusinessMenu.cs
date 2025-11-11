39763dd84388baacd1f1c1f88ef25b67817d8dcc
ElvisNilssonDev <Nilssonelvis@gmail.com>
2025-11-11 10:24:05 +0100

    Pushed maps

---
+++ UI/Menu/LoggedInMenu/BusinessMenu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookAChristmasHam.UI.Menu.LoggedInMenu
+{
+    internal class BusinessMenu
+    {
+    }
+}
